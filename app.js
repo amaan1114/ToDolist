d2be55c8d45759a45e28c298fe4b410b33557227
amaan1114 <amaan.zaidi05@gmail.com>
2025-06-06 14:31:33 +0530

    Updates

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path');
+
+const app = express()
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}))
+var list=[]
+
+app.use('/Resources', express.static(path.join(__dirname, 'Resources')));
+
+
+app.get('/',function(req,res){
+    res.render("index.ejs",{ejes:list})
+})
+
+
+app.post('/',function(req,res){
+    var item = req.body.InputField
+    if (item && item.trim() !== '') {
+        list.push(item.trim());
+    }
+    
+    
+    res.redirect('/')
+})
+
+app.post('/delete',function(req,res){
+    var inum = parseInt(req.body.index);
+     if (!isNaN(inum) && inum >= 0 && inum < list.length) {
+        list.splice(inum, 1);
+    }
+    res.redirect('/')
+
+})
+
+
+app.listen(8000,function(){
+    console.log("Started")
+})
+
+
