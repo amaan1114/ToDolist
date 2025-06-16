f256b92df60d1928b5fe5514e403c2ad03a3b6b4
amaan1114 <amaan.zaidi05@gmail.com>
2025-06-16 12:20:52 +0530

    Mongo db added

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
+const { stringify } = require('querystring');
 const port = process.env.PORT || 8080;
 const app = express()
 app.set("view engine","ejs")
 app.use(express.urlencoded({extended:true}))
-var list=[]
 
+mongoose.connect("mongodb://localhost:27017/todo")
+const trySchema  = new mongoose.Schema({
+    name:String,
+    check:Boolean
+});
+
+const item = mongoose.model("task",trySchema)
+
+const todo = new item({
+    name:"create some videos",
+    check:true
+})
+// todo.save()
 app.use('/Resources', express.static(path.join(__dirname, 'Resources')));
 
 
 app.get('/',function(req,res){
-    res.render("index.ejs",{ejes:list})
-})
+   item.find({})
+        .then(function(items1) {
+            res.render("index.ejs", { ejes: items1 });
+        })
+        .catch(function(err) {
+            console.error(err);
+            res.status(500).send("Error fetching items");
+        });
+    
+});
 
 
 app.post('/',function(req,res){
-    var item = req.body.InputField
-    if (item && item.trim() !== '') {
-        list.push(item.trim());
+    var iteminput = req.body.InputField
+    if (iteminput && iteminput.trim() !== '') {
+        const todo = new item({
+            name:iteminput.trim(),
+            
+        })   
+        todo.save()
     }
     
     
@@ -26,23 +52,47 @@ app.post('/',function(req,res){
 })
 
 app.post('/delete',function(req,res){
-    var inum = parseInt(req.body.index);
-    if (!isNaN(inum) && inum >= 0 && inum < list.length) {
-        list.splice(inum, 1);
+    var inum = req.body.index;
+    if(inum){
+        item.deleteOne({ name: inum })
+        .then(() => res.redirect("/"))
+        .catch(err => {
+            console.error("Delete failed:", err);
+            res.status(500).send("Failed to delete item");
+        });
+
     }
-    res.redirect('/')
+   
 
 })
 
 app.post('/submit-edit',function(req,res){
-    var inum = parseInt(req.body.index);
+    const oldName = req.body.oldName.trim();
+    const newName = req.body.newName.trim();
 
-    if (!isNaN(inum) && inum >= 0 && inum < list.length) {
-        list[inum]=req.body.text_edite;
+    if (!newName) {
+        return res.redirect("/");
     }
-   
-    res.redirect('/')
+
+    item.updateOne({ name: oldName }, { name: newName })
+        .then(() => res.redirect("/"))
+        .catch(err => {
+            console.error("Update failed:", err);
+            res.status(500).send("Failed to update item");
+        });
     
+})
+
+app.post('/checked',function(req,res){
+    var oldcheck=req.body.oldcheckName
+    const newcheck=  req.body.checkbox1=== "on"
+    item.updateOne({name:oldcheck},{check:newcheck})
+    .then(()=>res.redirect("/"))
+    .catch(err=>{
+        console.error("Update failed:", err);
+        res.status(500).send("Failed to update item");
+    })
+
 })
 
 app.listen(port,function(){
